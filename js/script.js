d7a4fb17f331c6c8d03c558d252e236dd4ccd161
nishanbose <nishan@umich.edu>
2015-05-04 22:45:50 -0400

    Updated Paper links on homepage

---
+++ js/script.js
@@ -0,0 +1,52 @@
+/* Custom Javascript for interactions
+ */
+
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+// Highlight the top nav as scrolling occurs
+$('body').scrollspy({
+    target: '.navbar-fixed-top'
+})
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
+});
+
+// Slider
+$(function($) {
+    "use strict";
+    $("body #slider").revolution(
+        {
+            delay:9000,
+            startheight:200,
+            startwidth:200,
+
+            thumbWidth:100,
+            thumbHeight:50,
+            thumbAmount:5,
+
+            onHoverStop:"off",
+            hideThumbs:200,
+            navigationType:"bullet",
+            navigationStyle:"round",
+            navigationArrows:"none",
+
+            touchenabled:"on",
+
+            navOffsetHorizontal:0,
+            navOffsetVertical:80,
+            shadow:undefined,
+            fullWidth:"on",
+            fullScreen:"on"
+        });
+});
